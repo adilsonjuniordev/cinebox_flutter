676a8801f7fe98f2bbfcb872fd70c98e8deb3ed8
Adilson <juninho_backer@hotmail.com>
2026-08-24 19:42:42 -0300

    Adiciona inicialização do Google Sign-In e atualiza dependências no pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:cinebox_flutter/cinebox_main_app.dart';
+import 'package:cinebox_flutter/config/env.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  GoogleSignIn.instance.initialize(serverClientId: Env.googleApiKeys);
+
   runApp(ProviderScope(child: const CineboxMainApp()));
 }
