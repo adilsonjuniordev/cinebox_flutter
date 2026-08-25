f9861ce9c54d64c1382d285203a59aed00747a28
Adilson <juninho_backer@hotmail.com>
2026-08-25 20:33:51 -0300

    feat: Adiciona barra de navegação inferior e animação na página inicial

---
+++ lib/ui/home/widgets/home_bottom_nav_bar.dart
@@ -0,0 +1,69 @@
+import 'package:cinebox_flutter/ui/core/themes/colors.dart';
+import 'package:cinebox_flutter/ui/core/themes/resource.dart';
+import 'package:cinebox_flutter/ui/home/widgets/home_bottom_nav_bar_item.dart';
+import 'package:flutter/material.dart';
+
+class HomeBottomNavBar extends StatefulWidget {
+  const HomeBottomNavBar({super.key});
+
+  @override
+  State<HomeBottomNavBar> createState() => _HomeBottomNavBarState();
+}
+
+class _HomeBottomNavBarState extends State<HomeBottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black38,
+            spreadRadius: 1,
+            blurRadius: 20,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: BottomNavigationBar(
+          enableFeedback: false,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          currentIndex: 0,
+          selectedItemColor: AppColors.redColor,
+          unselectedItemColor: AppColors.lightGrey,
+          selectedLabelStyle: TextStyle(
+            fontSize: 12,
+            color: AppColors.redColor,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 10,
+            color: AppColors.lightGrey,
+            fontWeight: FontWeight.w400,
+          ),
+          items: [
+            HomeBottomNavBarItem(
+              label: 'Filmes',
+              activeAsset: R.assetsImagesIconFilmeNavBarPng,
+              inactiveAsset: R.assetsImagesIconFilmeNavBarInativoPng,
+            ),
+            HomeBottomNavBarItem(
+              label: 'Favoritos',
+              activeAsset: R.assetsImagesIconHeartBnbPng,
+              inactiveAsset: R.assetsImagesIconHeartBnbInativoPng,
+            ),
+            HomeBottomNavBarItem(
+              label: 'Perfil',
+              activeAsset: R.assetsImagesIconProfileNavBarPng,
+              inactiveAsset: R.assetsImagesIconProfileNavBarInativoPng,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
